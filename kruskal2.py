8d95c35c780bf69fae077e9ea60decfdbc2643f2
Gevorg Khachatryan <48618597+Gevorg-Khachatryan@users.noreply.github.com>
2019-07-24 18:16:08 +0400

    Update kruskal2.py

--- kruskal2.py
+++ kruskal2.py
@@ -1,42 +1,55 @@
+
+def Dict(NumOfPeak):
+    x = []
+    z=[]
+    for i in range(NumOfPeak):
+        y = 's' + str(i + 1)
+        x.append(y)
+    for i in range(NumOfPeak):
+        y = 's' + str(i + 1)
+        z.append([y,dict.fromkeys(x)])
+    dic=dict(z)
+    return dic
+def DictPrint(x):
+    for peak in x.keys():
+            print (peak,x[peak])
 def kruskal(graph):
     assert type(graph)==dict
-
     nodes = graph.keys()
     visited = set()
     path = []
     next = None
-
-    while len(visited) < len(nodes):
-
+    while len(path) < len(nodes)-1:
         for s in nodes:
-            distance = 4
-            for d in nodes:
-                if s in visited and d in visited or s == d:
+            distance = 0
+            for d in sum[s]:
+                if s==d:
+                    sum[s][d]=0
+                if sum[s][d]==None:
+                    print("Enter ",s,d)
+                    sum[s][d]=int(input(":"))
+                    sum[d][s]=sum[s][d]
+                pathx = (s, d)
+                pathy = (d, s)
+                if pathx in path or pathy in path or s == d:
                     continue
-                if graph[s][d] < distance:
-                    distance = graph[s][d]
-                    pre = s
-                    next = d
+                if graph[s][d] > distance and distance!=0:
+                    continue
 
-        path.append((pre, next))
-        visited.add(pre)
-        visited.add(next)
+                distance = graph[s][d]
+                pre = s
+                next = d
+            if len(path)==len(nodes)-1:
+                continue
+            path.append((pre, next))
+            visited.add(pre)
+            visited.add(next)
 
     return path
-
-s1_s2, s1_s3, s1_s4, s1_s5= map(int, input("Enter s1_s2 ,s1_s3, s1_s4 , s1_s5  : ").split())
-s2_s3, s2_s4, s2_s5=map(int, input("Enter s2_s5 , s2_s10 , s2_s5 : ").split())
-s3_s4,s3_s5=map(int,input("Enter s3_s4 , s3_s5 : ").split())
-s4_s5=int(input("Enter s4_s5 : "))
-
-if __name__ == '__main__':
-    graph_dict = {  "s1":{"s1": 0, "s2": s1_s2, "s3": s1_s3, "s4": s1_s4, "s5":s1_s5},
-                    "s2":{"s1": s1_s2, "s2": 0, "s3": s2_s3, "s4": s2_s4, "s5":s2_s5},
-                    "s3":{"s1": s1_s3, "s2": s2_s3, "s3": 0, "s4":s3_s4, "s5":s3_s5},
-                    "s4":{"s1": s1_s4, "s2": s2_s4, "s3": s3_s4, "s4":0, "s5":s4_s5},
-                    "s5":{"s1": s1_s5, "s2": s2_s5, "s3": s3_s5, "s4":s4_s5, "s5":0},
-    }
-
-    path = kruskal(graph_dict)
-    print (path)
-
+num=int(input("Enter num of peak:"))
+sum=Dict(num)
+DictPrint(sum)
+path = kruskal(sum)
+print('***********')
+print (path)
+DictPrint(sum)
